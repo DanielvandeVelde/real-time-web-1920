fabeabe464eea55243bafda806a0fcf7ef58bf05
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-04-06 14:20:37 +0200

    basic chat functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,18 @@
 const socket = io();
 
 window.addEventListener("load", () => {
+  //Form listener for when messages are send.
   document.getElementById("form").addEventListener("submit", function(e) {
     e.preventDefault();
     socket.emit("chat message", document.getElementById("m").value);
     document.getElementById("m").value = "";
   });
+});
+
+//When receiving messages
+socket.on("chat message", function(msg) {
+  const node = document.createElement("li");
+  const textnode = document.createTextNode(msg);
+  node.appendChild(textnode);
+  document.getElementById("messages").append(node);
 });

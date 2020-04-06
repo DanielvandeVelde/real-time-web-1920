85f445b688529a9589bebe656cbdced8c73f4074
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2020-04-06 20:01:18 +0200

    proper styling for chatarea

--- static/js/script.js
+++ static/js/script.js
@@ -29,11 +29,39 @@ window.addEventListener("load", () => {
     });
 });
 
+function getTime() {
+  var currentdate = new Date();
+  var datetime =
+    String(currentdate.getDate()).padStart(2, "0") +
+    "/" +
+    String(currentdate.getMonth() + 1).padStart(2, "0") +
+    "/" +
+    currentdate.getFullYear() +
+    " " +
+    String(currentdate.getHours()).padStart(2, "0") +
+    ":" +
+    String(currentdate.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(currentdate.getSeconds()).padStart(2, "0");
+
+  return datetime;
+}
+
 //When receiving messages
 socket.on("chat message", function(data) {
   const node = document.createElement("li");
-  const textnode = document.createTextNode(data.nick + ": " + data.msg);
-  node.appendChild(textnode);
+  const nameElement = document.createElement("p");
+  const messageElement = document.createElement("blockquote");
+  const currentTime = document.createTextNode(getTime());
+  const timeSpan = document.createElement("span");
+  const messageText = document.createTextNode(data.msg);
+  const nameText = document.createTextNode(data.nick);
+  messageElement.appendChild(messageText);
+  nameElement.appendChild(nameText);
+  timeSpan.appendChild(currentTime);
+  nameElement.appendChild(timeSpan);
+  node.appendChild(nameElement);
+  node.appendChild(messageElement);
   document.getElementById("messages").append(node);
 });
 
